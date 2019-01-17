1bf24943bf7ac5c854ab3093572c3e08ccc46880
Hailey Ryu <hangyeol.ryu@lge.com>
2019-01-17 15:25:23 -0800

    updated with fps module.

--- performance/Picker.test.js
+++ performance/Picker.test.js
@@ -1,19 +1,19 @@
 const puppeteer = require('puppeteer');
+const FPS = require('./fps');
 
 function pad2(n) { return n < 10 ? '0' + n : n }
 const date = new Date();
 const formattedDate = date.getFullYear().toString() + pad2(date.getMonth() + 1) + pad2( date.getDate()) + pad2( date.getHours() ) + pad2( date.getMinutes() ) + pad2( date.getSeconds());
-
-// const timeout = 5000;
+const filename = `./performance/traces/picker_${formattedDate}.json`;
 
 describe(
 	'Picker', () => {
 		it('increment', async () => {
 			const browser = await puppeteer.launch({headless: true});
 			const page = await browser.newPage();
-			await page.tracing.start({ path: `./performance/traces/picker_${formattedDate}.json`, screenshots: true });
 			await page.goto('http://localhost:8080/picker');
-
+			await page.tracing.start({ path: filename, screenshots: false });
+			await page.waitFor(500);
 			await page.click('.Picker__incrementer___2mPTR');
 			await page.waitFor(200);
 			await page.click('.Picker__incrementer___2mPTR');
@@ -24,7 +24,11 @@ describe(
 			await page.click('.Picker__incrementer___2mPTR');
 
 			await page.tracing.stop();
-			// await browser.close();
+			await browser.close();
+
+			const {avgDuration, fps} = FPS(filename);
+			console.log(avgDuration, fps)
+			expect(Object.keys(fps).length).toBe(0);
 		})
 	}
 )
--- performance/Scroller.test.js
+++ performance/Scroller.test.js
@@ -2,29 +2,9 @@ const puppeteer = require('puppeteer');
 function pad2(n) { return n < 10 ? '0' + n : n }
 const date = new Date();
 const formattedDate = date.getFullYear().toString() + pad2(date.getMonth() + 1) + pad2( date.getDate()) + pad2( date.getHours() ) + pad2( date.getMinutes() ) + pad2( date.getSeconds());
+const filename = `./performance/traces/scroller_${formattedDate}.json`;
 
-// (async () => {
-// 	const browser = await puppeteer.launch({headless: true});
-// 	const page = await browser.newPage();
-// 	await page.tracing.start({ path: `./performance/traces/scroller_${formattedDate}.json`, screenshots: true });
-// 	await page.goto('http://localhost:8080/scroller');
-// 	await page.setViewport({
-//         width: 1920,
-//         height: 1080
-//     });
-
-//     await scrollAtPoint(page, [600, 400], [0, 100]);
-// 	await page.waitFor(200);
-// 	await scrollAtPoint(page, [600, 400], [0, 100]);
-// 	await page.waitFor(200);
-// 	await scrollAtPoint(page, [600, 400], [0, 100]);
-// 	await page.waitFor(200);
-// 	await scrollAtPoint(page, [600, 400], [0, 100]);
-// 	await page.waitFor(200);
-
-// 	await page.tracing.stop();
-// 	await browser.close();
-// })();
+const FPS = require('./fps');
 
 async function scrollAtPoint(
 	page,
@@ -49,12 +29,12 @@ describe(
     it('scrolls down', async() => {
       const browser = await puppeteer.launch({headless: true});
       const page = await browser.newPage();
-      await page.tracing.start({ path: `./performance/traces/scroller_${formattedDate}.json`, screenshots: true });
-      await page.goto('http://localhost:8080/scroller');
       await page.setViewport({
             width: 1920,
             height: 1080
         });
+      await page.goto('http://localhost:8080/scroller');
+      await page.tracing.start({ path: filename, screenshots: false });
 
       await scrollAtPoint(page, [600, 400], [0, 100]);
       await page.waitFor(200);
@@ -67,6 +47,10 @@ describe(
 
       await page.tracing.stop();
       await browser.close();
+
+      const {avgDuration, fps} = FPS(filename);
+      console.log(avgDuration, fps)
+      expect(Object.keys(fps).length).toBe(0);
     })
   }
 )
