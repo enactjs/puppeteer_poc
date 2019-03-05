5fffe748abe8a8d27cee2da5f0be5dfdec6ed04f
Stephen Choi <stephen.choi@lge.com>
2019-03-04 18:31:20 -0800

    ENYO-5870: Add test for verifying clientSize prop mounting and rendering perf
    Enact-DCO-1.0-Signed-off-by: Stephen Choi <stephen.choi@lge.com>

---
+++ performance/tests/VirtualListClientSize.test.js
@@ -0,0 +1,86 @@
+
+const puppeteer = require('puppeteer');
+const {Mount} = require('../TraceModel');
+const {getFileName} = require('../utils');
+const TestResults = require('../TestResults');
+
+describe('VirtualList clientSize prop', () => {
+	let browser;
+	let page;
+
+	beforeEach(async () => {
+		browser = await puppeteer.launch({headless: true});
+		page = await browser.newPage();
+		await page.setViewport({
+			width: 1920,
+			height: 1080
+		});
+
+		const client = await page.target().createCDPSession();
+		await client.send('Emulation.setCPUThrottlingRate', {rate: 6});
+	});
+
+	afterEach(async () => {
+		await browser.close();
+	});
+
+	describe('VirtualList js type', () => {
+		it('mount time without clientSize', async () => {
+			const filename = getFileName('VirtualList_withOutClientSize');
+
+			await page.tracing.start({path: filename, screenshots: false});
+			await page.goto('http://localhost:8080/virtualListClientSize?type=JS');
+			await page.waitFor(2000);
+
+			await page.tracing.stop();
+
+			const actual = Mount(filename, 'VirtualList');
+
+			TestResults.addResult({component: 'VirtualList', type: 'Mount', actualValue: actual});
+		});
+
+		it('mount time with clientSize', async () => {
+			const filename = getFileName('VirtualList_withClientSize');
+
+			await page.tracing.start({path: filename, screenshots: false});
+			await page.goto('http://localhost:8080/virtualListClientSize?clientSize=true&type=JS');
+			await page.waitFor(2000);
+
+			await page.tracing.stop();
+
+			const actual = Mount(filename, 'VirtualList');
+
+			TestResults.addResult({component: 'VirtualList', type: 'Mount', actualValue: actual});
+		});
+	});
+
+	describe('VirtualList native type', () => {
+		it('mount time without clientSize', async () => {
+			const filename = getFileName('VirtualListNative_withOutClientSize');
+
+			await page.tracing.start({path: filename, screenshots: false});
+			await page.goto('http://localhost:8080/virtualListClientSize');
+			await page.waitFor(2000);
+
+			await page.tracing.stop();
+
+			const actual = Mount(filename, 'VirtualListNative');
+
+			TestResults.addResult({component: 'VirtualListNative', type: 'Mount', actualValue: actual});
+		});
+
+		it('mount time with clientSize', async () => {
+			const filename = getFileName('VirtualListNative_withClientSize');
+
+			await page.tracing.start({path: filename, screenshots: false});
+			await page.goto('http://localhost:8080/virtualListClientSize?clientSize=true');
+			await page.waitFor(2000);
+
+			await page.tracing.stop();
+
+			const actual = Mount(filename, 'VirtualListNative');
+
+			TestResults.addResult({component: 'VirtualListNative', type: 'Mount', actualValue: actual});
+		});
+	});
+});
---
+++ src/views/VirtualListClientSize.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import kind from '@enact/core/kind';
+import VirtualListJS, {VirtualListNative} from '@enact/moonstone/VirtualList';
+import qs from 'qs';
+
+const items = [];
+
+// eslint-disable-next-line enact/prop-types
+const renderItem = ({index, ...rest}) => {
+	return (
+		<div {...rest}>
+			{items[index]}
+		</div>
+	);
+};
+
+for (let i = 0; i < 100; i++) {
+	items.push('Item ' + ('00' + i).slice(-3));
+}
+
+const itemSize = 60;
+
+const VirtualListClientSizeView = kind({
+	name: 'VirtualListClientSizeView',
+
+	render: ({location}) => {
+		const search = qs.parse(location.search, {ignoreQueryPrefix: true});
+		const clientSize = search.clientSize ? {clientWidth: 1280, clientHeight: 720} : undefined;
+		const VirtualList = search.type === 'JS' ? VirtualListJS : VirtualListNative;
+
+		return (<div style={{width: '1280px', height: '720px'}}>
+			<VirtualList
+				id="virtualList"
+				clientSize={clientSize}
+				dataSize={items.length}
+				focusableScrollbar
+				itemRenderer={renderItem}
+				itemSize={itemSize}
+			/>
+		</div>);
+	}
+});
+
+export default VirtualListClientSizeView;
