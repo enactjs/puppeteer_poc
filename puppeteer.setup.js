82c5c9ede1d209233048116f3c04140639094df2
Stephen Choi <stephen.choi@lge.com>
2019-03-06 19:27:21 -0800

    fix unintentional headless option change

--- puppeteer.setup.js
+++ puppeteer.setup.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 let browser, context;
 
 global.beforeAll(async () => {
-	browser = await puppeteer.launch({headless: false});
+	browser = await puppeteer.launch();
 	context = await browser.createIncognitoBrowserContext();
 });
 
